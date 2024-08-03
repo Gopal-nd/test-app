83805081daf8fea25175f5097f5deaf606101560
Gopal <gopalhelpyou@gmail.com>
2024-08-03 11:55:37 +0530

    removed some files

--- app/api/getfiles/route.ts
+++ app/api/getfiles/route.ts
@@ -1,12 +1,26 @@
-import { readdir } from "fs/promises";
+import { readdir, stat, mkdir } from "fs/promises";
 import { NextRequest, NextResponse } from "next/server";
 import { join } from "path";
 
-export async function GET(req:NextRequest,res:NextResponse){
-    const uploadeddir = join(process.cwd(),'public','newupload');
-    const files = await readdir(uploadeddir)
+export async function GET(req: NextRequest) {
+    const uploadeddir = join(process.cwd(), 'public', 'newupload');
 
-    return NextResponse.json({
-        success:true,file:files
-    })
+    try {
+        // Check if the directory exists
+        await stat(uploadeddir);
+    } catch (err:Error|any) {
+        if (err.code === 'ENOENT') {
+            // Directory does not exist, create it
+            await mkdir(uploadeddir, { recursive: true });
+        } else {
+            return NextResponse.json({ success: false, message: 'Error accessing upload directory' });
+        }
+    }
+
+    try {
+        const files = await readdir(uploadeddir);
+        return NextResponse.json({ success: true, files });
+    } catch (err) {
+        return NextResponse.json({ success: false, message: 'Error reading files from directory' });
+    }
 }
